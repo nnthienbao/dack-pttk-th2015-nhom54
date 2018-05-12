ba0e4c4adde313ee5ad1c094fbfd74da046bb5aa
Unknown <locphamduc96@gmail.com>
2018-05-13 03:10:10 +0700

    Hoàn thiện giao diện
    hoàn thiện giao diện ở tab: danh mục sách, độc giả, thông tin mượn-trả

--- QuanLyThuVien/DACK-PTTKPM/MainWindow.xaml.cs
+++ QuanLyThuVien/DACK-PTTKPM/MainWindow.xaml.cs
@@ -27,31 +27,103 @@ namespace DACK_PTTKPM
             this.MainArea.Content = new PageDSLoaiSach();
         }
 
+        //
+        // Quan ly danh muc sach
+        //
         private void btnPageDSLoaiSachClick(object sender, RoutedEventArgs e)
         {
             this.MainArea.Content = new PageDSLoaiSach();
         }
 
+        private void btnThemLoaiSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Them loai sach");
+        }
+
+        private void btnSuaLoaiSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sua loai sach");
+        }
+
+        private void btnXoaLoaiSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Xoa loai sach");
+        }
+
+        //
         private void btnPageDSNganhSachClick(object sender, RoutedEventArgs e)
         {
             this.MainArea.Content = new PageDSNganhSach();
         }
 
+        private void btnThemNganhSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Them Nganh sach");
+        }
+
+        private void btnSuaNganhSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sua Nganh sach");
+        }
+
+        private void btnXoaNganhSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Xoa Nganh sach");
+        }
+
+        //
         private void btnPageDSNhaXuatBanClick(object sender, RoutedEventArgs e)
         {
             this.MainArea.Content = new PageDSNhaXuatBan();
         }
 
+        private void btnThemNhaXuatBanClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Them nha xuat ban sach");
+        }
+
+        private void btnSuaNhaXuatBanClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sua nha xuat ban sach");
+        }
+
+        private void btnXoaNhaXuatBanClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Xoa nha xuat ban sach");
+        }
+
+        //
         private void btnPageDSSachClick(object sender, RoutedEventArgs e)
         {
             this.MainArea.Content = new PageDSSach();
         }
 
+        private void btnThemSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Them sach");
+        }
+
+        private void btnSuaSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Sua thong tin sach");
+        }
+
+        private void btnXoaSachClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Xoa thong tin sach");
+        }
+
+        //
+        //  Quan ly doc gia 
+        //
         private void btnPageDSDocGiaClick(object sender, RoutedEventArgs e)
         {
             this.MainArea.Content = new PageDSDocGia();
         }
 
+        //
+        //  Quan ly phieu muon tra
+        //
         private void btnPageDSPhieuMuonClick(object sender, RoutedEventArgs e)
         {
             this.MainArea.Content = new PageDSPhieuMuon();
@@ -62,6 +134,9 @@ namespace DACK_PTTKPM
             this.MainArea.Content = new PageDSPhieuTra();
         }
 
+        //
+        //  Bao cao - thong ke 
+        //
         private void btnPageDSViPhamClick(object sender, RoutedEventArgs e)
         {
             this.MainArea.Content = new PageDSViPham();
