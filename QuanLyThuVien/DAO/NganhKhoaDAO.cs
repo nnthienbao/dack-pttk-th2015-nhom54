01b540a3b95ff2822bebfd7e70bf7f785f9f0891
Unknown <nnthienbao040@gmail.com>
2018-05-14 23:53:41 +0700

    Chia cấu trúc 3 layer + test
    - Kèm linq

---
+++ QuanLyThuVien/DAO/NganhKhoaDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAO
+{
+    public class NganhKhoaDAO
+    {
+        public List<NganhKhoa> LayDanhSach()
+        {
+            List<NganhKhoa> nganhKhoas = new List<NganhKhoa>();
+            using (QLThuVienDataContext db = new QLThuVienDataContext())
+            {
+                nganhKhoas = db.NganhKhoas.Select(nk => nk).ToList();
+            }
+
+            return nganhKhoas;
+        }
+    }
+}
