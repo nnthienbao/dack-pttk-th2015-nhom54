0b84722456e356428d6f622b38363e5e1e02ebad
Unknown <nnthienbao040@gmail.com>
2018-05-17 00:06:59 +0700

    Giao diện thêm sách

--- QuanLyThuVien/DACK-PTTKPM/_qldanhmucsach/WindowThemSach.xaml.cs
+++ QuanLyThuVien/DACK-PTTKPM/_qldanhmucsach/WindowThemSach.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,24 @@ namespace DACK_PTTKPM
         public WindowThemSach()
         {
             InitializeComponent();
+        }
+
+        private void btn_ChonAnhMinhHoa_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG|All files (*.*)|*.*";
+            if(ofd.ShowDialog() == true)
+            {
+                string imgPath = ofd.FileName;
+                try
+                {
+                    this.img_AnhMinhHoa.Source = new BitmapImage(new Uri(imgPath));
+                    this.tb_DuongDanAnhMinhHoa.Text = imgPath;
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("Không hỗ trợ định dàng này", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
